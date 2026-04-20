de75475fed903dbee8013ba6484fe07e672d29fc
Dang Minh Tam <tamd10949@gmail.com>
2026-04-20 15:50:26 +0700

    Update Item

--- src/java/com/auction/shared/model/Item.java
+++ src/java/com/auction/shared/model/Item.java
@@ -1,3 +1,30 @@
-abstract class Item implements Entity{
+package com.auction.shared.model;
 
+import java.util.Date;
+import java.io.Serializable;
+
+public abstract class Item implements Entity, Serializable {
+    private double currentPrice;
+    private double minIncrement;
+    private Date startTime;
+    private Date endTime;
+    private String lastBidderId;
+
+    public Item() {
+    }
+
+    public double getCurrentPrice() { return currentPrice; }
+    public void setCurrentPrice(double currentPrice) { this.currentPrice = currentPrice; }
+
+    public double getMinIncrement() { return minIncrement; }
+    public void setMinIncrement(double minIncrement) { this.minIncrement = minIncrement; }
+
+    public Date getStartTime() { return startTime; }
+    public void setStartTime(Date startTime) { this.startTime = startTime; }
+
+    public Date getEndTime() { return endTime; }
+    public void setEndTime(Date endTime) { this.endTime = endTime; }
+
+    public String getLastBidderId() { return lastBidderId; }
+    public void setLastBidderId(String lastBidderId) { this.lastBidderId = lastBidderId; }
 }
