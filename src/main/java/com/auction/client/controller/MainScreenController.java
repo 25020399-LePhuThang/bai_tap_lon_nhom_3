f77e62a32cf2437f6225dc0a90550bbfc03d44d1
25020399-LePhuThang <25020399@vnu.edu.vn>
2026-05-10 21:52:05 +0700

    "Update Mainscreen, ItemDAO"

---
+++ src/main/java/com/auction/client/controller/MainScreenController.java
@@ -0,0 +1,178 @@
+package com.auction.client.controller;
+
+import com.auction.client.Network.NetworkClient;
+import com.auction.server.dao.ItemDAO;
+import com.auction.shared.model.Auction;
+import com.auction.shared.model.Item;
+import com.auction.shared.model.user.Bidder;
+import com.auction.shared.model.user.User;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class MainScreenController implements Initializable {
+
+    @FXML
+    private Button btnUpdate;
+    @FXML
+    private Button btnQuaylai2;
+    @FXML
+    private TableView<Item> tbvWillPresent;
+    @FXML
+    private TextField txtSearch1;
+    @FXML
+    private TableView<Item> tbvIsPresenting;
+    @FXML
+    private Label lblTime5;
+    @FXML
+    private Button btnDangXuat1;
+    @FXML
+    private Label lblName;
+    @FXML
+    private TableColumn<Item, String> NameColumn1;
+    @FXML
+    private TableColumn<Item, Integer> IDcolumn1;
+    @FXML
+    private TableColumn<Item, String> typeColumn1;
+    @FXML
+    private TableColumn<Item, Double> RecentPriceColumn;
+    @FXML
+    private TableColumn<Item, Date> EndTimeColumn1;
+    @FXML
+    private TableColumn<Item, String> NameColumn2;
+    @FXML
+    private TableColumn<Item, Integer> IDcolumn2;
+    @FXML
+    private TableColumn<Item, String> typeColumn2;
+    @FXML
+    private TableColumn<Item, Double> StartPriceColumn;
+    @FXML
+    private TableColumn<Item, Date> StartTimeColumn;
+    @FXML
+    private TableColumn<Item, Date> EndTimeColumn2;
+
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        clockInit();
+    }
+
+    public void clockInit() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss  dd/MM/yyyy");
+        Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
+            lblTime5.setText(LocalDateTime.now().format(formatter));
+        }), new KeyFrame(Duration.seconds(1)));
+
+        clock.setCycleCount(Timeline.INDEFINITE);
+        clock.play();
+    }
+
+    private void switchScence(ActionEvent event, String fxmlFile) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource(fxmlFile));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void toLoginScreen(ActionEvent event) throws IOException {
+        switchScence(event, "/SignInScreen.fxml");
+    }
+
+    public void Table1Presenting(URL url, ResourceBundle resourceBundle) {
+        NameColumn1.setCellValueFactory(new PropertyValueFactory<>("name"));
+        IDcolumn1.setCellValueFactory(new PropertyValueFactory<>("itemID"));
+        typeColumn1.setCellValueFactory(new PropertyValueFactory<>("type"));
+        RecentPriceColumn.setCellValueFactory(new PropertyValueFactory<>("currentPrice"));
+        EndTimeColumn1.setCellValueFactory(new PropertyValueFactory<>("endTime"));
+
+        loadtoTable1();
+
+        tbvIsPresenting.setOnMouseClicked(event -> {
+
+            if (event.getClickCount() == 2) {
+                Item selectedItem = tbvIsPresenting.getSelectionModel().getSelectedItem();
+
+                if (selectedItem != null) {
+                    System.out.println("Bạn muốn đấu giá vật phẩm: " + selectedItem.getName());
+                    openAuctionScreen(selectedItem);
+                }
+            }
+        });
+    }
+
+    //Kết nối với Server
+    public void loadtoTable1() {
+        List<Item> activeItems = NetworkClient.takeActiveItems();
+        ObservableList<Item> activeList = FXCollections.observableArrayList(activeItems);
+        tbvIsPresenting.setItems(activeList);
+    }
+
+    public void openAuctionScreen(Item chosenItem) {
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/AuctionDetail.fxml"));
+            Parent root = loader.load();
+            AuctionDetailController auctionDetailController = loader.getController();
+            auctionDetailController.setItemData(chosenItem);
+
+            Stage stage = new Stage();
+            stage.setTitle("Chi tiết: " + chosenItem.getName());
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void Table2Presenting(URL url, ResourceBundle resourceBundle) {
+        NameColumn2.setCellValueFactory(new PropertyValueFactory<>("name"));
+        IDcolumn2.setCellValueFactory(new PropertyValueFactory<>("itemID"));
+        typeColumn2.setCellValueFactory(new PropertyValueFactory<>("type"));
+        StartPriceColumn.setCellValueFactory(new PropertyValueFactory<>("startingPrice"));
+        StartTimeColumn.setCellValueFactory(new PropertyValueFactory<>("startTime"));
+        EndTimeColumn2.setCellValueFactory(new PropertyValueFactory<>("endTime"));
+
+        loadtoTable2();
+
+        tbvWillPresent.setOnMouseClicked(event -> {
+
+            if (event.getClickCount() == 2) {
+                Item selectedItem = tbvWillPresent.getSelectionModel().getSelectedItem();
+
+                if (selectedItem != null) {
+                    System.out.println("Bạn muốn đấu giá vật phẩm: " + selectedItem.getName());
+                    openAuctionScreen(selectedItem);
+                }
+            }
+        });
+    }
+
+
+    public void loadtoTable2() {
+        List<Item> activeItems = NetworkClient.takePreparedItems();
+        ObservableList<Item> activeList = FXCollections.observableArrayList(activeItems);
+        tbvWillPresent.setItems(activeList);
+    }
+}
--- src/main/java/com/auction/server/DAO/ItemDAO.java
+++ src/main/java/com/auction/server/DAO/ItemDAO.java
@@ -1,85 +1,248 @@
-//package com.auction.server.DAO;
+package com.auction.server.dao;
 
-//import com.auction.client.database.DatabaseManager;
-//import com.auction.shared.model.Item; // Đảm bảo bạn đã có class Item trong thư mục shared/model
-//import java.sql.Connection;
-//import java.sql.PreparedStatement;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class ItemDAO {
-//
-//    // 1. THÊM TÀI SẢN MỚI (Dùng khi Seller đăng bán một món hàng)
-//    public boolean insertItem(Item item) {
-//        // Giả sử bảng items không cần chèn id (vì AUTOINCREMENT) và status mặc định là 'ACTIVE'
-//        String sql = "INSERT INTO items (auction_id, item_name,description, start_price, step_price, seller_id, status) VALUES (?, ?, ?, ?, ?, ?)";
-//
-//        try (Connection conn = DatabaseManager.getConnection();
-//             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-//
-//            pstmt.setString(1, item.getId());
-//            pstmt.setString(2, item.getName());
-//            pstmt.setDouble(3, item.g);
-//            pstmt.setDouble(4, item.getStartingPrice()); // Vừa đăng lên thì giá hiện tại = giá khởi điểm
-//            pstmt.setInt(5, item.getSellerId());
-//            pstmt.setString(6, item.getEndTime()); // Lưu dưới dạng chuỗi (String) hoặc Timestamp tùy Database
-//
-//            int rowsAffected = pstmt.executeUpdate();
-//            return rowsAffected > 0;
-//
-//        } catch (SQLException e) {
-//            System.err.println("Lỗi khi thêm tài sản mới: " + e.getMessage());
-//            return false;
-//        }
-//    }
-//
-//    // 2. LẤY DANH SÁCH TÀI SẢN ĐANG ĐẤU GIÁ (Dùng để hiển thị lên giao diện cho Bidder xem)
-//    public List<Item> getActiveItems() {
-//        List<Item> itemList = new ArrayList<>();
-//        // Chỉ lấy những tài sản đang trong trạng thái mở đấu giá
-//        String sql = "SELECT * FROM items WHERE status = 'ACTIVE'";
-//
-//        try (Connection conn = DatabaseManager.getConnection();
-//             PreparedStatement pstmt = conn.prepareStatement(sql);
-//             ResultSet rs = pstmt.executeQuery()) {
-//
-//            while (rs.next()) {
-//                Item item = new Item();
-//                item.setId(rs.getInt("id"));
-//                item.setName(rs.getString("name"));
-//                item.setDescription(rs.getString("description"));
-//                item.setStartingPrice(rs.getDouble("starting_price"));
-//                item.setCurrentPrice(rs.getDouble("current_price"));
-//                item.setSellerId(rs.getInt("seller_id"));
-//                item.setEndTime(rs.getString("end_time"));
-//                item.setStatus(rs.getString("status"));
-//
-//                itemList.add(item);
-//            }
-//        } catch (SQLException e) {
-//            System.err.println("Lỗi khi lấy danh sách tài sản: " + e.getMessage());
-//        }
-//        return itemList;
-//    }
-//
-//    // 3. CẬP NHẬT GIÁ TÀI SẢN (Dùng khi có người đặt giá thành công)
-//    public boolean updateCurrentPrice(int itemId, double newPrice) {
-//        String sql = "UPDATE items SET current_price = ? WHERE id = ?";
-//
-//        try (Connection conn = DatabaseManager.getConnection();
-//             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-//
-//            pstmt.setDouble(1, newPrice);
-//            pstmt.setInt(2, itemId);
-//
-//            int rowsAffected = pstmt.executeUpdate();
-//            return rowsAffected > 0;
-//
-//        } catch (SQLException e) {
-//            System.err.println("Lỗi khi cập nhật giá: " + e.getMessage());
-//            return false;
-//        }
-//    }
-//}
+import com.auction.server.database.DatabaseManager;
+import com.auction.shared.model.Art;
+import com.auction.shared.model.Electronic;
+import com.auction.shared.model.Item; // Đảm bảo bạn đã có class Item trong thư mục shared/model
+import com.auction.shared.model.Vehicle;
+import com.auction.shared.model.user.Admin;
+import com.auction.shared.model.user.Bidder;
+import com.auction.shared.model.user.Seller;
+import com.auction.shared.model.user.User;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemDAO {
+
+    public boolean insertItem(Item item) {
+        String sql = "INSERT INTO items (item_name, start_price,currentprice, step_price, lastbidder_id, status,type,productImageURL) VALUES ( ?, ?, ?, ?, ?,?,?,?)";
+
+        try (Connection conn1 = DatabaseManager.getConnection();
+             PreparedStatement pstmt = conn1.prepareStatement(sql)) {
+
+            pstmt.setString(1, item.getId());
+            pstmt.setString(2, item.getName());
+            pstmt.setDouble(3, item.getStartingPrice());
+            pstmt.setDouble(4, item.getCurrentPrice()); // Vừa đăng lên thì giá hiện tại = giá khởi điểm
+            pstmt.setDouble(5, item.getMinIncrement());
+            pstmt.setString(6, item.getLastBidderId());
+            pstmt.setString(7, item.getStatus());
+            pstmt.setString(8, item.getType());
+            pstmt.setString(9, item.getProductImageURL());
+
+            int rowsAffected = pstmt.executeUpdate();
+            return rowsAffected > 0;
+
+        } catch (SQLException e) {
+            System.err.println("Lỗi khi thêm tài sản mới: " + e.getMessage());
+            return false;
+        }
+    }
+
+    // 2. LẤY DANH SÁCH TÀI SẢN ĐANG ĐẤU GIÁ
+    public List<Item> getActiveItems() {
+        List<Item> itemList = new ArrayList<>();
+        String sql2 = "SELECT * FROM items WHERE status = 'ACTIVE'";
+
+        try (Connection conn2 = DatabaseManager.getConnection();
+             PreparedStatement preparedStatement = conn2.prepareStatement(sql2);
+             ResultSet rs = preparedStatement.executeQuery()) {
+            {
+                try (ResultSet result = preparedStatement.executeQuery()) {
+                    while (rs.next()) {
+                        Item foundItem = null;
+                        String type = result.getString("type");
+
+                        if (type != null) {
+                            switch (type.toUpperCase()) {
+                                case "ELECTRONIC":
+                                    foundItem = new Electronic();
+                                    break;
+                                case "VEHICLE":
+                                    foundItem = new Vehicle();
+                                    break;
+                                case "ART":
+                                default:
+                                    foundItem = new Art();
+                                    break;
+                            }
+                        }
+                        if (foundItem != null) {
+                            foundItem.setId(rs.getString("item_id"));
+                            foundItem.setName(rs.getString("item_name"));
+                            foundItem.setStartingPrice(rs.getDouble("starting_price"));
+                            foundItem.setCurrentPrice(rs.getDouble("current_price"));
+                            foundItem.setMinIncrement(rs.getDouble("step_price"));
+                            foundItem.setLastBidderId(rs.getString("lastbidder_id"));
+                            foundItem.setEndTime(rs.getDate("EndTime"));
+                            foundItem.setStatus(rs.getString("status"));
+                            foundItem.setStartTime(rs.getDate("StartTime"));
+                            foundItem.setProductImageURL(rs.getString("productImageURL"));
+
+                            itemList.add(foundItem);
+                        }
+                    }
+                } catch (SQLException e) {
+                    System.err.println("Lỗi khi lấy danh sách tài sản: " + e.getMessage());
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return itemList;
+    }
+
+
+    // 3. CẬP NHẬT GIÁ TÀI SẢN
+    public boolean updateCurrentPrice(int itemId, double newPrice) {
+        String sql = "UPDATE items SET current_price = ? WHERE item_id = ?";
+
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setDouble(1, newPrice);
+            pstmt.setInt(2, itemId);
+
+            int rowsAffected = pstmt.executeUpdate();
+            return rowsAffected > 0;
+
+        } catch (SQLException e) {
+            System.err.println("Lỗi khi cập nhật giá: " + e.getMessage());
+            return false;
+        }
+
+    }
+
+    //Lấy danh sách tài sản đang được đấu giá
+    public List<Item> getPreparedItems() {
+        List<Item> itemList = new ArrayList<>();
+        String sql3 = "SELECT * FROM items WHERE status = 'PREPARED'";
+
+        try (Connection conn2 = DatabaseManager.getConnection();
+             PreparedStatement preparedStatement = conn2.prepareStatement(sql3);
+             ResultSet rs = preparedStatement.executeQuery()) {
+            {
+                try (ResultSet result = preparedStatement.executeQuery()) {
+                    while (rs.next()) {
+                        Item foundItem = null;
+                        String type = result.getString("type");
+
+                        if (type != null) {
+                            switch (type.toUpperCase()) {
+                                case "ELECTRONIC":
+                                    foundItem = new Electronic();
+                                    break;
+                                case "VEHICLE":
+                                    foundItem = new Vehicle();
+                                    break;
+                                case "ART":
+                                default:
+                                    foundItem = new Art();
+                                    break;
+                            }
+                        }
+                        if (foundItem != null) {
+                            foundItem.setId(rs.getString("item_id"));
+                            foundItem.setName(rs.getString("item_name"));
+                            foundItem.setStartingPrice(rs.getDouble("starting_price"));
+                            foundItem.setCurrentPrice(rs.getDouble("current_price"));
+                            foundItem.setMinIncrement(rs.getDouble("step_price"));
+                            foundItem.setLastBidderId(rs.getString("lastbidder_id"));
+                            foundItem.setEndTime(rs.getDate("EndTime"));
+                            foundItem.setStatus(rs.getString("status"));
+                            foundItem.setStartTime(rs.getDate("StartTime"));
+                            foundItem.setProductImageURL(rs.getString("productImageURL"));
+
+                            itemList.add(foundItem);
+                        }
+                    }
+                } catch (SQLException e) {
+                    System.err.println("Lỗi khi lấy danh sách tài sản: " + e.getMessage());
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return itemList;
+    }
+
+    //Lấy danh sách tài sản đã được đấu giá
+    public List<Item> getSoldItems() {
+        List<Item> itemList = new ArrayList<>();
+        String sql4 = "SELECT * FROM items WHERE status = 'SOLD'";
+
+        try (Connection conn2 = DatabaseManager.getConnection();
+             PreparedStatement preparedStatement = conn2.prepareStatement(sql4);
+             ResultSet rs = preparedStatement.executeQuery()) {
+            {
+                try (ResultSet result = preparedStatement.executeQuery()) {
+                    while (rs.next()) {
+                        Item foundItem = null;
+                        String type = result.getString("type");
+
+                        if (type != null) {
+                            switch (type.toUpperCase()) {
+                                case "ELECTRONIC":
+                                    foundItem = new Electronic();
+                                    break;
+                                case "VEHICLE":
+                                    foundItem = new Vehicle();
+                                    break;
+                                case "ART":
+                                default:
+                                    foundItem = new Art();
+                                    break;
+                            }
+                        }
+                        if (foundItem != null) {
+                            foundItem.setId(rs.getString("item_id"));
+                            foundItem.setName(rs.getString("item_name"));
+                            foundItem.setStartingPrice(rs.getDouble("starting_price"));
+                            foundItem.setCurrentPrice(rs.getDouble("current_price"));
+                            foundItem.setMinIncrement(rs.getDouble("step_price"));
+                            foundItem.setLastBidderId(rs.getString("lastbidder_id"));
+                            foundItem.setEndTime(rs.getDate("EndTime"));
+                            foundItem.setStatus(rs.getString("status"));
+                            foundItem.setStartTime(rs.getDate("StartTime"));
+                            foundItem.setProductImageURL(rs.getString("productImageURL"));
+
+                            itemList.add(foundItem);
+                        }
+                    }
+                } catch (SQLException e) {
+                    System.err.println("Lỗi khi lấy danh sách tài sản: " + e.getMessage());
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return itemList;
+    }
+
+    //Cập nhật status
+    public void updateToActive() throws SQLException {
+        String sql5 = "UPDATE items SET status = 'ACTIVE' WHERE status='PREPARED' AND startTime<=? AND ?<=endTime";
+        long TimeNow = System.currentTimeMillis();
+        Timestamp now = new Timestamp(TimeNow);
+
+        Connection conn2 = DatabaseManager.getConnection();
+        PreparedStatement preparedStatement = conn2.prepareStatement(sql5);
+
+        preparedStatement.setTimestamp(1, now);
+        preparedStatement.setTimestamp(2, now);
+    }
+
+    public void updateToSold() throws SQLException {
+        String sql5 = "UPDATE items SET status = 'SOLD' WHERE status='ACTIVE' AND ?>=endTime";
+        long TimeNow = System.currentTimeMillis();
+        Timestamp now = new Timestamp(TimeNow);
+
+        Connection conn2 = DatabaseManager.getConnection();
+        PreparedStatement preparedStatement = conn2.prepareStatement(sql5);
+
+        preparedStatement.setTimestamp(1, now);
+    }
+}
+}
