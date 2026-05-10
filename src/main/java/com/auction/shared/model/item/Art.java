3d15f869d8dceac8d5fac038647c85c1fca89819
25020399-LePhuThang <25020399@vnu.edu.vn>
2026-05-11 00:48:26 +0700

    cập nhật Item

---
+++ src/main/java/com/auction/shared/model/item/Art.java
@@ -0,0 +1,31 @@
+package com.auction.shared.model;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class Art extends Item {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    protected String author;
+    protected int creationYear;
+
+    public Art(String name, String author,int creationYear,double startingPrice, double minIncrement){
+        super(name,startingPrice,minIncrement);
+        this.author=author;
+        this.creationYear=creationYear;
+    }
+
+    public Art(){}
+
+    public String getAuthor(){ return author;}
+    public void setAuthor(String newAuthor){ author=newAuthor; }
+
+    public int getCreationYear(){ return creationYear;}
+    public void setCreationYear(int newCreationYear){ creationYear= newCreationYear; }
+
+    public void printInfo(){
+        System.out.println("Tên tác phẩm:\n"+getName()+"Tác giả:\n"+getAuthor()+"Năm tạo"+getCreationYear());
+    }
+
+}
---
+++ src/main/java/com/auction/shared/model/item/Vehicle.java
@@ -0,0 +1,39 @@
+package com.auction.shared.model;
+
+import java.io.Serial;
+
+public class Vehicle extends Item{
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private int warrantyPeriod;
+    private String brand;
+    private String fuelType;
+    private String engineCapacity;
+
+    public Vehicle(String name, String brand,String fuelType,String engineCapacity, int warrantyPeriod,double startingPrice, double minIncrement){
+        super(name,startingPrice,minIncrement);
+        this.brand=brand;
+        this.warrantyPeriod=warrantyPeriod;
+        this.engineCapacity=engineCapacity;
+        this.fuelType=fuelType;
+    }
+
+    public Vehicle(){}
+
+    public int getWarrantyPeriod(){ return warrantyPeriod;}
+    public void setWarrantyPeriod(int newWarrantyPeriod){ warrantyPeriod=newWarrantyPeriod; }
+
+    public String getBrand(){ return brand;}
+    public void setBrand(String newBrand){ brand=newBrand; }
+
+    public String getFuelType(){ return fuelType;}
+    public void setFuelType(String newFuelType){ fuelType=newFuelType; }
+
+    public String getEngineCapacity(){ return engineCapacity;}
+    public void setEngineCapacity(String newEngineCapacity){ engineCapacity=newEngineCapacity; }
+
+    public void printInfo(){
+        System.out.println("Tên phương tiện:\n"+getName()+" chỗ\n"+"Hãng:\n"+getBrand()+"Loại nhiên liệu:\n"+getFuelType()+"Dung tích động cơ/Công suất:\n"+getEngineCapacity()+"Thời hạn bảo hành còn: "+getWarrantyPeriod()+" tháng");
+    }
+}
