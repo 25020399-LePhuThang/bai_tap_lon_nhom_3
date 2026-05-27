5fb44d0c317cf104f60b1d77d0eee9998f0a9255
Dang Minh Tam <tamd10949@gmail.com>
2026-05-27 21:26:53 +0700

    Tách AntiSnipingPolicy

--- src/main/java/com/auction/server/controller/AntiSnipingPolicy.java
+++ src/main/java/com/auction/server/controller/AntiSnipingPolicy.java
@@ -1,7 +1,20 @@
 package com.auction.server.controller;
 
+import com.auction.shared.model.item.Item;
+
 public class AntiSnipingPolicy {
-    // Phần 3.2.3: Thuật toán gia hạn phiên đấu giá
-    public void checkAndExtendDuration(int auctionId) {
+
+    public boolean apply(Item item) {
+        // Tính thời gian còn lại (ms)
+        long timeLeft = item.getEndTime().getTime() - System.currentTimeMillis();
+
+        // Nếu còn dưới 30 giây thì gia hạn
+        if (timeLeft > 0 && timeLeft < 30000) {
+            long newEndTime = item.getEndTime().getTime() + 60000; // Cộng thêm 60s
+            item.setEndTime(new java.util.Date(newEndTime));
+            System.out.println(">>> He thong: Phat hien Sniping! Gia han them 60s.");
+            return true; // Trả về true để báo là có gia hạn
+        }
+        return false; // Không cần gia hạn
     }
 }
